d1c0a223168cea05fa6a811a56ed0952fd0ecb4b
Sergio85 <chechucarrasco@gmail.com>
2019-10-10 18:43:58 +0200

    Creamos archivo de configuración de rutas(routes/project.js) que usaremos para trabajar con el controlador de la entidad Project

---
+++ routes/project.js
@@ -0,0 +1,19 @@
+/** Fichero de configuración de las rutas del controlador /controller/project.js **/
+'use strict'
+
+//Cargamos el módulo de Express para poder trabajar con las rutas
+var express = require('express');
+//Cargamos el controlador que hemos creado /controllers/project.js para la entidad Project
+var ProjectController = require('../controllers/project');
+//Cargamos el método o servicio de Express Router que contiene métodos para trabajar con las rutas
+var router = express.Router();
+
+
+//** Creamos las rutas para el controlador /controllers/project **
+//Creamos la ruta /home por el método get al cuál le asignamos el método home: que creado en el controlador
+router.get('/home', ProjectController.home);
+//Creamos ruta /test por el método post
+router.post('/test', ProjectController.test);
+
+//Exportamos el nuestro módulo router con toda la configuración de rutas
+module.exports = router;
